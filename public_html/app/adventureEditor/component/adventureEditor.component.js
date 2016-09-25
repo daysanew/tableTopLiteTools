495a0e1db8963814b4b38bea11cd30a3d91b5d7e
daysanew <charlieleverett@gmail.com>
2016-09-25 07:54:33 -0500

    Revert "started adventure editor"
    This reverts commit 0b4e966fae086afed622b27e2a0ffc2213676a08.

--- public_html/app/adventureEditor/component/adventureEditor.component.js
+++
@@ -1,8 +0,0 @@
-angular.module('storyCard').component('adventureEditor', {
-    templateUrl: './app/adventureEditor/templates/adventureEditor.html',
-    controller: [
-        function AdventureEditorController() {
-        }
-    ]
-}
-);
