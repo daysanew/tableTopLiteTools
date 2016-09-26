7efd227e4039d31ec0a14ff2d0a12ca99f86138a
daysanew <charlieleverett@gmail.com>
2016-09-26 07:21:09 -0500

    added model to adventure edit page

--- public_html/app/AdventureManager/component/adventureManagerComponent.js
+++ public_html/app/AdventureManager/component/adventureManagerComponent.js
@@ -2,7 +2,10 @@ angular.module('tableTopTools').component('adventureManager', {
     templateUrl: './app/AdventureManager/templates/editAdventure.html',
     controller: [
         function AdventureManagerController() {
+            var self = this;
             
+            self.adventure = {name: '', description: '', ruleSet: ''};
+            self.ruleSets = [{value: 1, name: "D20 Fantasy"}];
         }
     ]
 }
