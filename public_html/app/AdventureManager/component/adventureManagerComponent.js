843eb16d88aff4931005924ff2576cf3c25dcd5a
daysanew <charlieleverett@gmail.com>
2016-11-11 07:03:23 -0600

    Adding tools to create adventures

--- public_html/app/AdventureManager/component/adventureManagerComponent.js
+++ public_html/app/AdventureManager/component/adventureManagerComponent.js
@@ -5,6 +5,13 @@ angular.module('tableTopTools').component('adventureManager', {
             var self = this;
             self.adventure = {adventureId: '', name: '', description: '', ruleSet: ''};
             self.adventure.adventureId = $routeParams.adventureId;
+            if(self.adventure.adventureId){
+                var adventurePromise = Adventure.get({adventureId: self.adventure.adventureId});
+                adventurePromise.$promise.then(function(result){
+                   self.adventure = result.adventure; 
+                });
+            }
+            
             self.ruleSets = [{value: 1, name: "D20 Fantasy"}];
             
             self.upsertAdventure = function(){
---
+++ public_html/app/StoryManager/component/storyManagerComponent.js
@@ -0,0 +1,10 @@
+angular.module('tableTopTools').component('storyManager', {
+    templateUrl: './app/StoryManager/templates/story.html',
+    controller: ['$routeParams',
+        function StoryManagerController($routeParams) {
+            var self = this;
+            
+        }
+    ]
+}
+);
